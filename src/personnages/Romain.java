df62935d5061f70cb6f7b7a227aca6c305030d48
PsyKusss <mael.cuillier@univ-tlse3.fr>
2024-09-27 08:38:46 +0200

    Invariant pre post conditions

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -6,6 +6,7 @@ public class Romain {
 	
 	public Romain(String nom, int force) {
 		this.nom = nom;
+		assert force > 0 : "La force d'un Romain doit être positive.";
 		this.force = force;
 	}
 
@@ -22,15 +23,18 @@ public class Romain {
 	}
 	
 	public void recevoirCoup(int forceCoup) {
+		assert force > 0 : "La force d'un Romain est positive.";
+		int ancienneForce = force;
 		force -= forceCoup;
 		if (force > 0) {
 			parler( "Aie");
 		} else {
 			parler("J'abandonne ...");
 		}		
+		assert ancienneForce > force : "La force d'un Romain a diminué.";
 	}
 	
 	public static void main(String[] args) {
-		
+		Romain minus = new Romain("Minus",6);
 	}
 }
